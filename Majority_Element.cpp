c738e3e413794ca23c7461df8dcf3d9a18535819
Hiyaa <2106hiyaa@gmail.com>
2025-07-12 23:33:17 +0530

    Initial Commit: Added 189) Rotate Array and 169) Majority Element

---
+++ Majority_Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate=0;
+
+        for(int x: nums){
+            if (count == 0) {
+            candidate = x;
+            }
+        count += (x == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
